9ee599f7f025d7d40c70028688d961b3eaeded11
Sasha Sohrabi <sasha@nxte.nl>
2020-02-05 19:02:48 +0200

    added time filters

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="movie-filter">
     <h2>Filter results</h2>
+    <h3>By time of day</h3>
+    <div class="filter-group">
+      <check-filter
+        v-for="time in times"
+        category="time"
+        :key="time"
+        :title="time"
+        @check-filter="checkFilter"
+      />
+    </div>
+    <h3>By genre</h3>
     <div class="filter-group">
       <check-filter
         v-for="genre in genres"
+        category="genre"
         :key="genre"
         :title="genre"
         @check-filter="checkFilter"
@@ -12,8 +24,10 @@
   </div>
 </template>
 <script>
-import CheckFilter from "./CheckFilter.vue";
 import genres from "../util/genres";
+import times from "../util/times";
+import CheckFilter from "./CheckFilter.vue";
+
 export default {
   components: {
     CheckFilter
@@ -25,7 +39,8 @@ export default {
   },
   data() {
     return {
-      genres
+      genres,
+      times
     };
   }
 };
