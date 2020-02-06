b3d3629cee03e100c34a4403461a829c80e28e8e
Sasha Sohrabi <sasha@nxte.nl>
2020-02-06 19:01:35 +0200

    setting up vue router

--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./style.scss";
+import VueRouter from "vue-router";
+import routes from "./util/router";
 import VueResource from "vue-resource";
-
 import moment from "moment-timezone";
+
+import "./style.scss";
+
+
+Vue.use(VueRouter);
+const router = new VueRouter({ routes });
+
+
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, "$moment", { value: moment });
 
-Vue.use(VueResource);
 
 const bus = new Vue();
 Object.defineProperty(Vue.prototype, "$bus", { value: bus });
 
+
+Vue.use(VueResource);
+
+
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
